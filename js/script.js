57df1292ab882459052e0a3b856a7a2ff92f14f5
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2024-12-01 23:58:29 +0100

    JS CASI TERMINADO CSS HECHO

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const searchInput = document.getElementById("searchInput");
 const search = document.getElementById("searchBtn");
 const anterPag = document.getElementById("prevBtn");
 const proxPag = document.getElementById("nextBtn");
@@ -7,6 +8,7 @@ let offset = 0;
 let limit = 10;
 let totalPaginas = 132;
 let paginaActual = 1;
+let buscadorPokemon = []; // Acá me estoy almacenando mis pokemones del buscdor para mostrarlos con la funcion cargar pokemon pero con el parama¡etro de la filtración
 
 
 const pokemonFetch = async () => {
@@ -23,8 +25,11 @@ const pokemonFetch = async () => {
         const detalles = await Promise.all(
             data.results.map((pokemon) => pokemonFetchData(pokemon.url))
         );
+        
+        
 
-        cargarPokemones(detalles)
+        buscadorPokemon = detalles;
+        cargarPokemones(buscadorPokemon); // Aca antes el parametro era detalle pero ahora lo actualizamos ya que en la linea de arriba lo igualamos al buscador 
         console.log("Data Completa Pokémon:", detalles);
 
         return detalles;
@@ -58,12 +63,12 @@ function cargarPokemones (detalles) {
     detalles.forEach(pokemon => {
         if (pokemon) {
             app.innerHTML += `
-                <div class="pokemon-card">
-                    <h2>${pokemon.name}</h2>
+                <div class="pokeDatos">
+                    <h2>${pokemon.name.toUpperCase()}</h2>
                     <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
                     <p>Tipo: ${pokemon.types.map(type => type.type.name).join(", ").toUpperCase()}</p>
-                    <p>Altura: ${pokemon.height} m</p>
-                    <p>Peso: ${pokemon.weight} kg</p>
+                    <p>Altura: ${pokemon.height} Dm</p>
+                    <p>Peso: ${pokemon.weight} Hg</p>
                 </div>
             `;
         }
@@ -72,7 +77,7 @@ function cargarPokemones (detalles) {
 
 proxPag.addEventListener("click", () => {
     if (paginaActual < totalPaginas){
-        offset += limit; // se actualiza offset en el valor de limit
+        offset += limit; // Acá me actualiza offset en el valor de limit
         paginaActual++
 
         pokemonFetch(); 
@@ -81,11 +86,33 @@ proxPag.addEventListener("click", () => {
 
 anterPag.addEventListener("click", () => {
     if (paginaActual > 1){
-        offset -= limit; // se actualiza offset en el valor de limit
+        offset -= limit; // Acá me actualiza offset en el valor de limit
         paginaActual--
 
         pokemonFetch(); 
     }
 })
 
+search.addEventListener("click", () => {
+    const valorIngresado = searchInput.value.toLowerCase();
+
+    const pokemonFiltro = buscadorPokemon.filter(pokemon => 
+        // pokemon me representa cada pokemon
+        pokemon.name.toLowerCase().includes(valorIngresado) || 
+        pokemon.types.some(type => type.type.name.toLowerCase().includes(valorIngresado)) 
+        // .some me verifica si al enos alguno cumple parametro ya que en type tengo dos valores (EJ: fuego, volador)
+);
+if (pokemonFiltro.length === 0) {// compruebo que este vacio ya que no se ingresaria ningun valoe
+    alert ("No se ha encontrado al Pokemon!")}
+
+    cargarPokemones(pokemonFiltro);
+});
+
+reseteo.addEventListener("click", () => {
+    app.innerHTML = ``
+    offset = 0
+    pokemonFetch()
+})
+
 pokemonFetch();
+
